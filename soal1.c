f7b1d76e02c2a0164ac6e50035adfc88ffc12c10
akidnana0 <aziizadityatama@gmail.com>
2026-04-13 08:53:10 +0700

    commit ketiga

--- soal1.c
+++ soal1.c
@@ -1,9 +1,9 @@
 /** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
 *   Modul                       : 01 – Overview of C Language
-*   Hari dan Tanggal            : Seninn, 12 April 2026
+*   Hari dan Tanggal            : Senin, 12 April 2026
 *   Nama (NIM)                  : Aziiz Adityatama (13224070)
 *   Nama File                   : soal1.c
-*   Deskripsi                   : Memverifikasi data input dari peneliti
+*   Deskripsi                   : Memverifikasi status akhir data input dari peneliti
 */
 
 #include <stdio.h>
@@ -22,14 +22,14 @@ int main(){
     else if(suhu >= 390){
         printf("KARANTINA\n");
     }
-    else if(izin == 1 && (jam < 6 || jam > 20)){
+    else if(izin == 1 && ((jam < 6 ) || (jam > 20))){
         printf("TOLAK\n");
     }
     else if(izin == 1){
         printf("MASUK\n");
     }
     else if(izin == 2 && (radiasi <= 2 && (jam >= 8 && jam <=18))){
-        printf("PEMERIKSAAN\n");
+        printf("MASUK\n");
     }
     else if(izin == 2){
         printf("PEMERIKSAAN\n");
